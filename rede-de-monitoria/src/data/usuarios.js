8cb8c4c135ed2fabc672c79d9949b1443c782299
Pedro Luís <pllv2109@gmail.com>
2026-05-07 01:34:34 -0300

    feat: criação do arquivo das turmas

--- rede-de-monitoria/src/data/usuarios.js
+++ rede-de-monitoria/src/data/usuarios.js
@@ -1,11 +1,15 @@
 const users = [
     {
-        matricula: '12345',
+        nome: 'Pedro Luís Lima Vieira',
+        matricula: 12345,
         senha: 54321,
+        cargo: 'M',
     },
     {
-        matricula: '67890',
-        senha: '2468',
+        nome: 'Pedro Vieira',
+        matricula: 24680,
+        senha: 'abcde',
+        cargo: 'A',
     }
 ]
 
