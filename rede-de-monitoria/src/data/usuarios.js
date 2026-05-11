07b509f566e20d958541ce11aaca3af410603c2b
PedroLuís <pllv2109@gmail.com>
2026-05-11 02:09:28 -0300

    fix: corrigindo a logo das páginas

--- rede-de-monitoria/src/data/usuarios.js
+++ rede-de-monitoria/src/data/usuarios.js
@@ -1,16 +1,3 @@
-const users = [
-    {
-        nome: 'Pedro Luís Lima Vieira',
-        matricula: 12345,
-        senha: 54321,
-        cargo: 'M',
-    },
-    {
-        nome: 'Pedro Vieira',
-        matricula: 24680,
-        senha: 'abcde',
-        cargo: 'A',
-    }
-]
+const usersArray = []
 
-export default users;
+localStorage.setItem("usuarios", JSON.stringify(usersArray))
