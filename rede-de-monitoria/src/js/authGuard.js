e20b1e7a719bd475330e7c6e0743e243371a6e2e
Pedro Luís <pllv2109@gmail.com>
2026-05-13 12:59:07 -0300

    fix: redefinindo caminho dos scripts

--- rede-de-monitoria/src/js/authGuard.js
+++
@@ -1,6 +0,0 @@
-const logado = sessionStorage.getItem("userLogado")
-
-if (logado === null){
-    alert("Faça login para acessar a home.")
-    window.location.href = 'cadastro.html'
-}
--- rede-de-monitoria/src/js/home.js
+++
@@ -1,6 +0,0 @@
-const perfil = document.getElementById('profile')
-const menu = document.getElementById('menu')
-
-perfil.addEventListener('click', function dropdown(){
-    menu.classList.toggle('hidden')
-})
