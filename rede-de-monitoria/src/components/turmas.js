ed3d25acc9d22dfb05f977d2ee3bffc30656b715
Pedro Luís <pllv2109@gmail.com>
2026-05-13 15:51:19 -0300

    feat: criação do array de disciplinas

---
+++ rede-de-monitoria/src/components/turmas.js
@@ -0,0 +1,20 @@
+const disciplinasP1 = [
+    {
+        nome: "PWEB 1",
+        monitor: "A definir",
+    },
+    {
+        nome: "Intro. à Programação",
+        monitor: "A definir",
+    },
+    {
+        nome: "Arq. e Org. de Comp.",
+        monitor: "A definir",
+    },
+    {
+        nome: "Intro. à Eng. Software",
+        monitor: "A definir",
+    }
+];
+
+export {disciplinasP1};
