b3148325e3e172228b2efc779f7341139624b578
Pedro Luís <pllv2109@gmail.com>
2026-07-01 03:15:50 -0300

    feat: cria regex para nome e curso da turma

--- rede-monitoria-next/src/schemas/turmaSchema.ts
+++ rede-monitoria-next/src/schemas/turmaSchema.ts
@@ -1,8 +1,14 @@
 import {z} from "zod";
 
+const nomeRegex = /^[A-Za-zÀ-ÿ0-9\s-]+$/;
+const cursoRegex= /^[A-Za-zÀ-ÿ\s]+$/;
+
 export const turmaSchema = z.object({
-    nome: z.string().min(3, "Informe um nome válido"),
-    curso: z.string().min(3, "Informe um nome válido."),
+    nome: z.string().min(3, "Informe um nome válido")
+    .regex(nomeRegex, "Apenas letras, números, espaço e hífen são permitidos."),
+
+    curso: z.string().min(3, "Informe um nome válido.")
+    .regex(cursoRegex, "Apenas letras são permitidos."),
 })
 
 export type TurmaFormData = z.infer<typeof turmaSchema>;
